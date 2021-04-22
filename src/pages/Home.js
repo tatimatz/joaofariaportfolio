c16bc3d7de1e851f85706c09ba41e595e410c368
Tatiana Matzenbacher <tatimatz@gmail.com>
2021-04-22 10:58:58 +0200

    home settings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import ButtonsBottom from "../components/ButtonsBottom";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
+import Mainphoto from "../assets/fonts/mainphoto.jpg";
 
 const Home = () => {
   return (
     <div>
       <div className="home">
-      <Navigation />
-      <SocialNetwork />
+      <img src={Mainphoto} alt={"joão"} className="joao-background" />
+        <Navigation />
+        <SocialNetwork />
         <div className="home-main">
           <div className="main-content">
             <h1>JOÃO FARIA</h1>

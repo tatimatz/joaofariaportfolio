10c5051ce5180ab9dec8b2ba0c4f0128a1f4ca8d
Tatiana Matzenbacher <tatimatz@gmail.com>
2021-04-28 16:48:51 +0200

    Contact page done. Still need to adjust responsive design

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Profile from "../assets/profile.jpeg";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Bio = () => {
   return (
     <div className="bio-main">
       <div className="bio-content">
         <h1>joão faria</h1>
-        </div>
-        <div className="img-bio-content">
-          <div className="img-bio-container hover">
-            <span>
-              <p className="bio-text">
+        <p className="bio-text">
                 João Faria nasceu em Bagé, em 1958, onde cresceu, estudou e
                 começou, desde muito pequeno, mais longe do que a memória
                 registra, a desenhar e a pintar, porque era assim mais fácil de
@@ -26,11 +24,48 @@ const Bio = () => {
                 parte da complexidade e da inexatidão da matéria humana. Suas
                 obras são assim, o retrato do sincero.
               </p>
-            </span>
+        </div>
+        <div className="img-bio-content">
+          <div className="img-bio-container hover">
             <img src={Profile} alt={"João Faria"} className="bio-img" />
           </div>
         </div>
-      
+        <div className="contact-infos">
+          <div>
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0665544332" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Téléphone copié");
+                  }}
+                >
+                  06 65 54 43 32
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+
+          <div>
+              <div className="content">
+                  <h4>email</h4>
+                  <CopyToClipboard text="jbage@hotmail.fr" className="hover">
+                      <p style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Email copié");
+                  }}>jbage@hotmail.fr</p>
+                  </CopyToClipboard>
+              </div>
+          </div>
+          <div>
+          <div className="content">
+          <SocialNetwork />
+          </div>
+          </div>
+    </div>
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,53 +1,18 @@
 import React from "react";
 import Navigation from "../components/Navigation";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import SocialNetwork from "../components/SocialNetwork";
-import ButtonsBottom from "../components/ButtonsBottom";
 import Bio from "../components/Bio";
 
 const Contact = () => {
   return (
     <main>
       <div className="contact">
+
         <Navigation />
         <Bio />
-        <div className="contact-infos">
-          
-          <div>
-            <div className="content">
-              <h4>téléphone</h4>
-              <CopyToClipboard text="0665544332" className="hover">
-                <p
-                  style={{ cursor: "pointer" }}
-                  className="clipboard"
-                  onClick={() => {
-                    alert("Téléphone copié");
-                  }}
-                >
-                  06 65 54 43 32
-                </p>
-              </CopyToClipboard>
-            </div>
-          </div>
 
-          <div>
-              <div className="content">
-                  <h4>email</h4>
-                  <CopyToClipboard text="jbage@hotmail.fr" className="hover">
-                      <p style={{ cursor: "pointer" }}
-                  className="clipboard"
-                  onClick={() => {
-                    alert("Email copié");
-                  }}>jbage@hotmail.fr</p>
-                  </CopyToClipboard>
-              </div>
-          </div>
-          <SocialNetwork />
-          <div className="credits">
+          <footer className="credits">
               <p id="name-credits">Tatiana Matzenbacher - 2021</p>
-          </div>
-        </div>
-        <ButtonsBottom left={"project-2"} />
+          </footer>
       </div>
     </main>
   );
